8096116e9f1f1c36cf06fc8bdcf57904016b3716
nshvijay <vijaypalkumawat2020@gmail.com>
2025-12-25 00:35:43 +0530

    day1

---
+++ operator.py
@@ -0,0 +1,7 @@
+#in any case when / is used the final output will always be a float value
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
---
+++ sum.py
@@ -0,0 +1,10 @@
+#this is for printing sum
+#and we now also know that # is used to comment any statement
+"""this is can also be used to comment,
+generally used for multi line comment"""
+a=2
+b=5
+sum =a+b
+diff=b-a
+print("sum is",sum)
+print("difference is",diff)
