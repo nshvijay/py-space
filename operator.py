ea43e87df3d88b71c86b2a3c0b8a041ad4247ed4
nshvijay <vijaypalkumawat2020@gmail.com>
2025-12-25 18:17:56 +0530

    operatos \o

--- operator.py
+++ operator.py
@@ -1,7 +1,34 @@
 #in any case when / is used the final output will always be a float value
+#** is used for power
 a=10
 b=5
 print(a+b)
 print(a-b)
 print(a*b)
 print(a/b)
+print(a%b)
+print(a**b)
+
+
+#realtion operators true false type
+
+print(a == b)#False
+print(a != b)#True
+print(a >= b)#True
+print(a <= b)#False
+
+
+#assignment operators used to assign values to a variable
+
+x = 10
+x = x + 10 # += 
+print("x is :",x)
+
+
+#logical operators (not, and ,or) 
+a=10
+b=8
+value =True
+print(not value)
+print(a>b and value)
+print(a<b or not value)
