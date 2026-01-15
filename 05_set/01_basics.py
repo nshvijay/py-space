5ccbeef02555b3f71cb04517b76af8ffbdc60c1e
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-15 23:34:45 +0530

    set basics

---
+++ 05_set/01_basics.py
@@ -0,0 +1,15 @@
+#unodered, unique & immutable 
+# ignores the dublicate values
+set0 = {1,2,3,4}
+set1={1,2,2,2,2,3,4}
+print(set0)
+print(set1) #result will be same 
+print(type(set))
+
+#unodered bcz doesnt follow order ex.
+set2={1,2,5,6,7,"hello",3}
+print(set2)         #order is not same 
+set3= set()
+print(set3)     #syntax to define empty set 
+
+# aovid naming your set as "set" 
