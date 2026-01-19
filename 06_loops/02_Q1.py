bb78a09538420720ce8ba0d2140a0632672ab52b
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-19 15:50:09 +0530

    Q2

---
+++ 06_loops/02_Q1.py
@@ -0,0 +1,11 @@
+# print numbers from 1 to 100 and reverse too.
+print("normal ordere")
+count = 1
+while count<=100 :
+    print(count)
+    count+=1
+print("reverse order")
+count-=1
+while count>=1 :
+    print(count)
+    count-=1
---
+++ 06_loops/03_Q2.py
@@ -0,0 +1,7 @@
+#table of n
+n=int(input("enter n:"))
+print("Table of n:")
+count =1
+while count<=10 :
+    print(n*count)
+    count+=1
