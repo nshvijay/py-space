c56e2d43d497024ae3f8285ddbc85bc031de29c9
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-19 15:25:34 +0530

    loops basics

---
+++ 06_loops/01_basicsyn.py
@@ -0,0 +1,10 @@
+# while loop 
+
+count = 0
+while count <4 :
+    print("Run",count)
+    count+=1
+
+print(count)
+
+# stoping conditions to stop look or it will create infinite loop which is not good.
