ae329d2f8a2e5f4b9934bdf3151a00e2b75acdd9
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-19 19:34:41 +0530

    forbasics

---
+++ 06_loops/06_forloop.py
@@ -0,0 +1,12 @@
+#for loop
+name = ["vijay","nishtha","kanav","keshav"]
+for nme in name:
+    print(nme)
+
+number = [1,2,3,4,5,4,3,7,8,9]
+for num in number :
+    print(num)
+
+me ="Vijay"
+for let in me:
+    print(let)
